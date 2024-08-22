5daf9cd7ea1acbdb003d0dffe23bf7bda664a90b
Nixkey <pokeloquendo102@gmail.com>
2024-08-22 18:37:44 -0300

    fix

--- TP 2/tres.js
+++ TP 2/tres.js
@@ -3,60 +3,66 @@ const API = "https://jsonplaceholder.typicode.com/users"
 // 1. Consumo de Datos desde una API:
 console.log("\nEjercicio 1:")
 
-const obtenerUsuarios = function(api){
+const obtenerUsuarios = async function(api){
     const peticion = fetch(api)
 
-    peticion.then(resp => resp.json())
-    .then(users => {
-        users.forEach(usuario => {
-            console.log(usuario) 
-        });
-    }).catch(console.warn)
+    const usuarios = peticion.then(resp => resp.json())
+    .then(users => users)
+        // users.forEach(usuario => {
+        //     console.log(usuario) 
+        // });
+    .catch(console.warn)
+    return usuarios
 }
 
-obtenerUsuarios(API)
+obtenerUsuarios(API).then(console.log)
+//const usuarios = await obtenerUsuarios(API).then(console.log)
+//console.log(usuarios)
 
 // 2. Procesamiento de Datos de una API:
 console.log("\nEjercicio 2:")
-// const imprimirNombresDeUsuarios = function(users){
-//     users.forEach(user => {
-//         console.log(user.username)
-//     })
-// }
-const imprimirNombresDeUsuarios = function(api){
-    const peticion = fetch(api)
-
-    peticion.then(resp => resp.json())
-    .then(users => {
-        users.forEach(usuario => {
-            console.log(usuario.name) 
-        });
-    }).catch(console.warn)
-}
-imprimirNombresDeUsuarios(API)
-
-console.log("\nEjercicio 3:")
-// 3. Autenticación Simulada
-const persona = {
-    username: "Bret",
-    name: "Leanne Graham"
-}
-
-const autenticarUsuario = function(credencial){
-    const peticion = fetch(API)
-
-    return peticion.then(resp => resp.json())
-    .then(users =>{
-        users.forEach(usuario =>{
-            if ((credencial.username === usuario.username) && (credencial.name === usuario.name)){
-                return true
-            }
-        })
-        return false
+const imprimirNombresDeUsuarios = function(users){
+    users.forEach(user => {
+        console.log(user.username)
     })
 }
 
-console.log(autenticarUsuario(persona))
 
-console.log("\nEjercicio 4:")
+obtenerUsuarios(API).then(imprimirNombresDeUsuarios)
+// const imprimirNombresDeUsuarios = function(api){
+//     const peticion = fetch(api)
+
+//     peticion.then(resp => resp.json())
+//     .then(users => {
+//         users.forEach(usuario => {
+//             console.log(usuario.name) 
+//         });
+//     }).catch(console.warn)
+// }
+// imprimirNombresDeUsuarios(API)
+
+// console.log("\nEjercicio 3:")
+// // 3. Autenticación Simulada
+// const persona = {
+//     username: "Bret",
+//     name: "Leanne Graham"
+// }
+
+// const autenticarUsuario = function(credencial){
+//     const peticion = fetch(API)
+
+//     return peticion.then(resp => resp.json())
+//     .then(users =>{
+//         users.forEach(usuario =>{
+//             if ((credencial.username === usuario.username) && (credencial.name === usuario.name)){
+//                 return true
+//             }
+//         })
+//         return false
+//     })
+// }
+
+// console.log(autenticarUsuario(persona))
+
+// console.log("\nEjercicio 4:")
 //4. Transformación de Datos
